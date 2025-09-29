27624f8d9f8e611b4f2f6f4a2fe9cc06b62fe97c
Kenil-Mangukiya <kenilmangukiya033@gmail.com>
2025-09-29 15:36:27 +0530

    Add message template functions and order generation: implement sendFlowTemp, sendTemp, and sendTempImage functions for WhatsApp messaging, and enhance order and invoice number generation using the Conversation model.

--- public/src/function/index.js
+++ public/src/function/index.js
@@ -1,4 +1,5 @@
 import axios from "axios"; 
+import { Conversation } from "../db/models/index.js";
 
 const sendTextMsg = async (from, text) => {
   var config = {
@@ -57,142 +58,142 @@ const markAsRead = async (msgId) => {
     });
 }
 
-// const sendFlowTemp = async (from, name) => {
-//     try {
-//       const options = {
-//         method: "POST",
-//         url: `${process.env.FBWA_URL}/send-message`,
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json, application/xml",
-//           Authorization: `Bearer ${process.env.FBWA_KEY}`,
-//         },
-//         data: {
-//           messaging_product: "whatsapp",
-//           recipient_type: "individual",
-//           to: from,
-//           type: "template",
-//           template: {
-//             name: name,
-//             language: {
-//               code: "en",
-//             },
-//             components: [
-//               {
-//                 type: "button",
-//                 sub_type: "flow",
-//                 index: "0",
-//               },
-//             ],
-//           },
-//         },
-//       };
-  
-//       const { data } = await axios.request(options);
-//     } catch (error) {
-//       console.log({ error: error?.message });
-//     }
-// };
-  
-// const sendTemp = async (from, name) => {
-//     try {
-//       const options = {
-//         method: "POST",
-//         url: `${process.env.FBWA_URL}/send-message`,
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json, application/xml",
-//           Authorization: `Bearer ${process.env.FBWA_KEY}`,
-//         },
-//         data: {
-//           messaging_product: "whatsapp",
-//           recipient_type: "individual",
-//           to: from,
-//           type: "template",
-//           template: {
-//             name: name,
-//             language: {
-//               code: "en_US",
-//             },
-//           },
-//         },
-//       };
-  
-//       const { data } = await axios.request(options);
-  
-//       console.log(data);
-//     } catch (error) {
-//       console.log({ error: error });
-//     }
-// };
-  
-// const sendTempImage = async (from, name, image) => {
-//     try {
-//       const options = {
-//         method: "POST",
-//         url: `${process.env.FBWA_URL}/send-message`,
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json, application/xml",
-//           Authorization: `Bearer ${process.env.FBWA_KEY}`,
-//         },
-//         data: {
-//           messaging_product: "whatsapp",
-//           recipient_type: "individual",
-//           to: from,
-//           type: "template",
-//           template: {
-//             name: name,
-//             language: {
-//               code: "en_US",
-//             },
-//             components: [
-//               {
-//                 type: "HEADER",
-//                 parameters: [
-//                   {
-//                     type: "IMAGE",
-//                     image: {
-//                       link: image,
-//                     },
-//                   },
-//                 ],
-//               },
-//             ],
-//           },
-//         },
-//       };
-  
-//       const { data } = await axios.request(options);
-  
-//       console.log(data);
-//     } catch (error) {
-//       console.log({ error: JSON.stringify(error) });
-//     }
-// };
-  
-// const orderNoGen = async () => {
-//     const orderData = await Order.findOne({ order: [["id", "DESC"]] });
-//     if (orderData) {
-//       const last_order = orderData?.orderNo;
-//       const last = parseInt(last_order.match(/\d+/)[0], 10);
-//       const next = last + 1;
-//       const paddedNumber = String(next).padStart(7, "0");
-//       return `ORD${paddedNumber}`;
-//     }
-//     return `ORD0000001`;
-// };
-  
-// const invoiceNoGen = async () => {
-//     const invoice = await Invoice.findOne({ order: [["id", "DESC"]] });
-//     if (invoice) {
-//       const last_invoice = invoice?.invoiceNo;
-//       const last = parseInt(last_invoice.match(/\d+/)[0], 10);
-//       const next = last + 1;
-//       const paddedNumber = String(next).padStart(7, "0");
-//       return `INV${paddedNumber}`;
-//     }
-//     return `INV0000001`;
-// };
+const sendFlowTemp = async (from, name) => {
+  try {
+    const options = {
+      method: "POST",
+      url: `${process.env.FBWA_URL}/send-message`,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, application/xml",
+        Authorization: `Bearer ${process.env.UPMATRIX_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: from,
+        type: "template",
+        template: {
+          name: name,
+          language: {
+            code: "en",
+          },
+          components: [
+            {
+              type: "button",
+              sub_type: "flow",
+              index: "0",
+            },
+          ],
+        },
+      },
+    };
 
-export { sendTextMsg, markAsRead };
+    const { data } = await axios.request(options);
+  } catch (error) {
+    console.log({ error: error?.message });
+  }
+};
+
+const sendTemp = async (from, name) => {
+  try {
+    const options = {
+      method: "POST",
+      url: `${process.env.FBWA_URL}/send-message`,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, application/xml",
+        Authorization: `Bearer ${process.env.UPMATRIX_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: from,
+        type: "template",
+        template: {
+          name: name,
+          language: {
+            code: "en_US",
+          },
+        },
+      },
+    };
+
+    const { data } = await axios.request(options);
+
+    console.log(data);
+  } catch (error) {
+    console.log({ error: error });
+  }
+};
+
+const sendTempImage = async (from, name, image) => {
+  try {
+    const options = {
+      method: "POST",
+      url: `${process.env.FBWA_URL}/send-message`,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, application/xml",
+        Authorization: `Bearer ${process.env.UPMATRIX_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: from,
+        type: "template",
+        template: {
+          name: name,
+          language: {
+            code: "en_US",
+          },
+          components: [
+            {
+              type: "HEADER",
+              parameters: [
+                {
+                  type: "IMAGE",
+                  image: {
+                    link: image,
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+
+    const { data } = await axios.request(options);
+
+    console.log(data);
+  } catch (error) {
+    console.log({ error: JSON.stringify(error) });
+  }
+};
+
+const orderNoGen = async () => {
+  const orderData = await Conversation.findOne({ order: [["id", "DESC"]] });
+  if (orderData) {
+    const last_order = orderData?.message_content;
+    const last = parseInt(last_order.match(/\d+/)[0], 10);
+    const next = last + 1;
+    const paddedNumber = String(next).padStart(7, "0");
+    return `ORD${paddedNumber}`;
+  }
+  return `ORD0000001`;
+};
+
+const invoiceNoGen = async () => {
+  const invoice = await Conversation.findOne({ order: [["id", "DESC"]] });
+  if (invoice) {
+    const last_invoice = invoice?.message_content;
+    const last = parseInt(last_invoice.match(/\d+/)[0], 10);
+    const next = last + 1;
+    const paddedNumber = String(next).padStart(7, "0");
+    return `INV${paddedNumber}`;
+  }
+  return `INV0000001`;
+};
+
+export { sendTextMsg, markAsRead, sendFlowTemp, sendTemp, sendTempImage, orderNoGen, invoiceNoGen };
